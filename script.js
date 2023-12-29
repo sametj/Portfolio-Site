155da1242d460d7d24634f741a43c3bfe396360d
sametj <mosesjames2000@gmail.com>
2023-12-29 00:15:29 -0500

    testing swipe

--- script.js
+++ script.js
@@ -3,27 +3,11 @@ const aboutMe = document.querySelector(".about-me");
 const aboutMeButton = document.querySelector(".experience-button");
 const active = document.querySelector(".active");
 const projects = document.querySelectorAll(".project");
-const project = document.querySelector(".project");
+var currentIndex = 0;
 
-const aboutMeText = ` Results-driven Software Engineering enthusiast with a strong
-foundation in Java programming, Object-Oriented Programming (OOP),
-and proficiency in Agile methodologies. Highly adaptable and
-committed to contributing innovative solutions to complex software
-development challenges. Seeking opportunities to further expand
-expertise and make a meaningful impact in the world of software
-engineering.`;
-const experienceText = `CUNY School of Medicine                                                                                                                                       New York, NY                                                                                                                                                               
-  Cuny Office Assistant                                                                                                                                             02/2023 - 07/2023
-  Enhanced data organization and accuracy through Excel spreadsheet creation and editing
-  Arranged Zoom meetings for seamless virtual interactions.
-  Operated CUNYFirst website for requisition management
-  
-  New York City Department of Social Services 						                              New York, NY
-  IT Intern 											           07/2022 - 08/2022
-  Queried and edited Oracle database for required data and built Excel Trackers using ODBC for data visualization
-  Used Jira and Confluence to link development issues for bug identification
-  Participated in daily SCRUM meetings for task update.
-  `;
+let left = `translate3d(-60%, 0, -220px)`;
+let right = `translate3d(60%, 0, -220px)`;
+let middle = `translate3d(0, 0, 0)`;
 
 // Add a mousemove event listener
 profileCard.addEventListener("mousemove", function (e) {
@@ -42,22 +26,6 @@ aboutMe.addEventListener("mouseleave", function () {
   resetTilt(this);
 });
 
-aboutMeButton.addEventListener("click", function () {
-  if (aboutMe.childNodes[1].innerText === "About Me") {
-    aboutMe.childNodes[1].innerText = "Experience";
-    aboutMeButton.innerHTML = "About Me";
-    aboutMe.childNodes[3].innerText = experienceText;
-  } else if (aboutMe.childNodes[1].innerText === "Experience") {
-    aboutMe.childNodes[1].innerText = "About Me";
-    aboutMeButton.innerHTML = "Experience";
-    aboutMe.childNodes[3].innerText = aboutMeText;
-  }
-});
-
-window.addEventListener("load", function () {
-  aboutMe.childNodes[3].innerText = aboutMeText;
-});
-
 // Tilt the element when mouse moves
 function tilt(container, e) {
   const tiltX =
@@ -74,8 +42,6 @@ function resetTilt(container) {
   container.style.transform = "";
 }
 
-var currentIndex = 0;
-
 function updateCarousel() {
   for (var i = 0; i < projects.length; i++) {
     if (i === currentIndex) {
@@ -85,29 +51,73 @@ function updateCarousel() {
     }
   }
 }
-let left = `translate3d(-60%, 0, -220px)`;
-let right = `translate3d(60%, 0, -220px)`;
-let middle = `translate3d(0, 0, 0)`;
+
+function rotateCorousel(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[0].classList.contains("active")) {
+      array[1].style.transform = right;
+      array[0].style.transform = middle;
+      array[2].style.transform = left;
+    } else if (array[1].classList.contains("active")) {
+      array[2].style.transform = right;
+      array[1].style.transform = middle;
+      array[0].style.transform = left;
+    } else if (array[2].classList.contains("active")) {
+      array[0].style.transform = right;
+      array[1].style.transform = left;
+      array[2].style.transform = middle;
+    }
+  }
+}
 
 projects.forEach(function (project, index) {
   project.addEventListener("click", function () {
-    for (var i = 0; i < projects.length; i++) {
-      if (projects[0].classList.contains("active")) {
-        projects[1].style.transform = right;
-        projects[0].style.transform = middle;
-        projects[2].style.transform = left;
-      } else if (projects[1].classList.contains("active")) {
-        projects[2].style.transform = right;
-        projects[1].style.transform = middle;
-        projects[0].style.transform = left;
-      } else if (projects[2].classList.contains("active")) {
-        projects[0].style.transform = right;
-        projects[1].style.transform = left;
-        projects[2].style.transform = middle;
-      }
-    }
     currentIndex = index;
-
     updateCarousel();
+    rotateCorousel(projects);
   });
 });
+
+//Rototate carousel on mobile swipe
+let touchStartX = 0;
+let touchEndX = 0;
+
+function handleTouchStart(e) {
+  touchStartX = e.changedTouches[0].screenX;
+}
+
+function handleTouchMove(e) {
+  touchEndX = e.changedTouches[0].screenX;
+}
+
+function handleTouchEnd() {
+  if (touchEndX < touchStartX) {
+    console.log("Swiped left");
+    projects.forEach(function (project, index) {
+      project.addEventListener("touchend", function () {
+        currentIndex = index;
+        updateCarousel();
+        rotateCorousel(projects);
+      });
+    });
+  }
+
+  if (touchEndX > touchStartX) {
+    console.log("Swiped right");
+    projects.forEach(function (project, index) {
+      project.addEventListener("touchend", function () {
+        currentIndex = index;
+        updateCarousel();
+        rotateCorousel(projects);
+      });
+    });
+  }
+
+  // Reset
+  touchStartX = 0;
+  touchEndX = 0;
+}
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+document.addEventListener("touchend", handleTouchEnd, false);
