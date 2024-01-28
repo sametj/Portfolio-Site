d1954b71f321859ba054e28cb6ed30c4d1498fe2
sametj <mosesjames2000@gmail.com>
2024-01-28 16:49:52 -0500

    FIX: Too much tilt

--- src/js/3d.js
+++ src/js/3d.js
@@ -1,7 +1,5 @@
 import "../style.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GUI } from "lil-gui";
 
 const sizes = {
   width: document.body.scrollWidth,
